4145a5a339c5c7960c3e8f42893c05442f1f4132
engalar <engalar@gmail.com>
2022-01-11 14:51:33 +0800

    Initial commit

---
+++ typings/CascaderProps.d.ts
@@ -0,0 +1,37 @@
+/**
+ * This file was generated from Cascader.xml
+ * WARNING: All changes made to this file will be overwritten
+ * @author Mendix Widgets Team
+ */
+import { CSSProperties } from "react";
+import { EditableValue } from "mendix";
+
+interface CommonProps {
+    name: string;
+    class: string;
+    tabIndex: number;
+
+    uniqueid: string;
+    friendlyId?: string;
+    mxform: mxui.lib.form._FormBase;
+    mxObject?: mendix.lib.MxObject;
+    readOnly: boolean;
+    style: string;
+}
+
+interface _W {}
+
+export interface CascaderContainerProps extends CommonProps, _W {
+    myString: string;
+}
+
+export interface CascaderPreviewProps extends _W {
+    class: string;
+    style: string;
+    styleObject: CSSProperties;
+    myString: string;
+}
+
+export interface VisibilityMap {
+    [P in _W]: boolean;
+}
